6d6c68601c5935143dcc81928309b3c6bfcd763b
Seth J. Williams <sjwilliams3@students.unwsp.edu>
2024-11-01 12:18:48 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -4,10 +4,17 @@
 # Write a program that displays the number of names that are stored in the file.
 
 def count_file_lines():
-    ######################
-    # Add your code here #
-    ######################
-    print('In the count_file_lines function')
+    infile = open('names.txt', 'r')
+
+    file_contents = infile.read()
+    infile.close()
+    print(file_contents)
+    count = 0
+    my_string = file_contents
+    for ch in my_string:
+        count += 1
+    count_file_lines = my_string
+    print(f'In the count_file_lines function')
 
 
 
